b503f5a1cb0011105a311719ae78df58da1157b3
centurianii <centurianii@yahoo.co.uk>
2017-12-22 14:15:00 +0200

    Update test.php

--- test.php
+++ test.php
@@ -9,20 +9,29 @@ $token->set('log', true);
 $client = new GoogleHttpClient($token, $token->get('log'));
 
 /* STEP 1 */
-// follow next link and Google redirects with code=...
+// follow next link and Google will make a redirection with code=...
 echo $client->createAuthUrl();
 echo '<br/><a href="'.$client->createAuthUrl().'">authenticate</a>';
 
+// comment STEP 1
+// replace variable value with the value of the url part '&code=...'
 //$code = "4/e_H1Yj...meQnE";
+// uncomment STEP 2
 
 /* STEP 2 */
-// look at log file
 //$client->authenticate($code);
 
+// comment STEP 2
+// look at log file
+// copy from log file the 'access_token' value and replace argument
+// uncomment STEP 3
+
 /* STEP 3 */
-// copy from log file 'access_token' value
 //$client->verify("ya29.GlsmBd3F796AR0...z0rdz");
 
+// comment STEP 3
+// copy from log file the 'access_token' value and replace argument
+// uncomment STEP 4
+
 /* STEP 4 */
-// copy from log file 'access_token' value
 //$client->userInfo("ya29.Glsg...MW9r2");
