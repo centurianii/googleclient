8e5f1bca764c32e0b52183d348aca9c05f5d6ceb
centurianii <centurianii@yahoo.co.uk>
2018-01-09 13:13:54 +0200

    Update GoogleHttpClient.php
    Added cert path file and CURLOPT_RESOLVE.

--- GoogleHttpClient.php
+++ GoogleHttpClient.php
@@ -7,7 +7,7 @@ namespace auth;
 use g3\Utils;
 
 /**
- * The GoogleHttpClient is a helper class that is called from other commands.
+ * The GoogleHttpClient is a helper class that is called from @see GoogleCommand.
  * 
  * As this class is called between different requests, it needs access to a state
  * storage mechanism like sessions.
@@ -117,22 +117,34 @@ class GoogleHttpClient {
       $client_secret  = $this->token->get('client_secret');
       $redirect_uri = $this->token->get('redirect_uri');
       $url = $this->token->get('token_endpoint');
-      $curlPost = 'client_id=' . $client_id . '&client_secret=' . $client_secret . '&redirect_uri=' . $redirect_uri . '&code='. $code . '&grant_type=authorization_code';
+      $this->token->set('code', $code); // see getFields()
+      $curlPost = $this->getFields($this->token->get('token_fields'));
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL, $url);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
       curl_setopt($ch, CURLOPT_POST, 1);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+      curl_setopt($ch, CURLOPT_CAINFO, $this->token->get('cert_path'));
+      //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+      //curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
+      curl_setopt($ch, CURLOPT_RESOLVE, [$this->token->get('accounts.google.com')]);
       curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
+      if ($this->log) {
+         curl_setopt($ch, CURLOPT_VERBOSE, true);
+         $fh = \fopen($this->token->get('curl_log'), 'w+');
+         curl_setopt($ch, CURLOPT_STDERR, $fh);
+      }
       $buffer = curl_exec($ch);
       $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
       curl_close($ch);
-      $data = \json_decode($buffer, true);
+      if ($this->log) {
+         \fclose($fh);
+      }
       if ($http_code != 200) {
-         $log = __CLASS__ . '::authenticate() error: http code not 200. Responded: '.print_r($data, true);
+         $log = __CLASS__ . '::authenticate() error: http code not 200. Responded: '.print_r($buffer, true);
          $return = false;
       } else {
+         $data = \json_decode($buffer, true);
          $this->auth = $data;
          $return = true;
          $log = __CLASS__ . '::authenticate() returns '.$return.' and sets this->auth='.print_r($data, true);
@@ -163,22 +175,32 @@ class GoogleHttpClient {
       }
       $url = $this->token->get('verify_endpoint');
       $curlPost = 'access_token='. $access_token;
-      //$curlPost = \http_build_query(array('access_token' => $access_token));
-      //$curlPost = array('access_token' => $access_token);
       $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL, $url.'?'.$curlPost);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-      //curl_setopt($ch, CURLOPT_VERBOSE, true);
+      curl_setopt($ch, CURLOPT_URL, $url.'?'.$curlPost);
+      curl_setopt($ch, CURLOPT_CAINFO, $this->token->get('cert_path'));
+      //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+      //curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
+      curl_setopt($ch, CURLOPT_RESOLVE, [$this->token->get('www.googleapis.com')]);
+      if ($this->log) {
+         curl_setopt($ch, CURLOPT_VERBOSE, true);
+         $fh = \fopen($this->token->get('curl_log'), 'a+');
+         curl_setopt($ch, CURLOPT_STDERR, $fh);
+      }
       $buffer = curl_exec($ch);
+      $info = curl_getinfo($ch);
       $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
       curl_close($ch);
-      $data = \json_decode($buffer, true);
+      if ($this->log) {
+         \fclose($fh);
+      }
       if ($http_code != 200) {
-         $log = __CLASS__ . '::verify() error: http code not 200. Responded: '.print_r($data, true);
+         $log = __CLASS__ . '::verify() error: http code not 200. Responded: '.print_r($buffer, true);
          $return = false;
       } else {
+         $buffer = (isset($info["header_size"]))? \substr($buffer, $info["header_size"]) : "";
+         $data = \json_decode($buffer, true);
          $this->verify = $data;
          $log = __CLASS__ . '::verify() sets this->verify='.print_r($data, true);
          $return = true;
@@ -212,8 +234,10 @@ class GoogleHttpClient {
       if (!$user) {
          $arr[] = 'argument $user is null';
       }
-      if ($verify_token && !isset($verify_token['user_id'])) {
-         $arr[] = 'key \'user_id\' at argument $verify_token does not exist';
+      if ($verify_token && !isset($verify_token['user_id']) && !isset($verify_token['sub'])) {
+         $arr[] = 'key \'user_id\' or \'sub\' at argument $verify_token does not exist';
+      } elseif (isset($verify_token['sub'])) {
+         $verify_token['user_id'] = $verify_token['sub'];
       }
       if ($user && !isset($user['id'])) {
          $arr[] = 'key \'id\' at argument $user does not exist';
@@ -224,8 +248,10 @@ class GoogleHttpClient {
       if ($user && !isset($user['email'])) {
          $arr[] = 'key \'email\' at argument $user does not exist';
       }
-      if ($verify_token && !isset($verify_token['issued_to'])) {
-         $arr[] = 'key \'issued_to\' at argument $verify_token does not exist';
+      if ($verify_token && !isset($verify_token['issued_to']) && !isset($verify_token['aud'])) {
+         $arr[] = 'key \'issued_to\' or \'aud\' at argument $verify_token does not exist';
+      } elseif (isset($verify_token['aud'])) {
+         $verify_token['issued_to'] = $verify_token['aud'];
       }
       if(\count($arr)) {
          $log = __CLASS__ . '::validate() ' . \implode(', ', $arr);
@@ -273,8 +299,6 @@ class GoogleHttpClient {
             error_log(__CLASS__ . '::refresh() error: $refresh_token is null.');
          return false;
       }
-      $client_id = $this->token->get('client_id');
-      $client_secret  = $this->token->get('client_secret'); 
       $url = $this->token->get('refresh_endpoint');
       $this->token->set('refresh_token', $refresh_token); // see getFields()
       $curlPost = $this->getFields($this->token->get('refresh_fields'));
@@ -282,17 +306,28 @@ class GoogleHttpClient {
       curl_setopt($ch, CURLOPT_URL, $url);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
       curl_setopt($ch, CURLOPT_POST, 1);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+      curl_setopt($ch, CURLOPT_CAINFO, $this->token->get('cert_path'));
+      //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+      //curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
+      curl_setopt($ch, CURLOPT_RESOLVE, [$this->token->get('accounts.google.com')]);
       curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
+      if ($this->log) {
+         curl_setopt($ch, CURLOPT_VERBOSE, true);
+         $fh = \fopen($this->token->get('curl_log'), 'a+');
+         curl_setopt($ch, CURLOPT_STDERR, $fh);
+      }
       $buffer = curl_exec($ch);
       $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
       curl_close($ch);
-      $data = \json_decode($buffer, true);
+      if ($this->log) {
+         \fclose($fh);
+      }
       if ($http_code != 200) {
-         $log = __CLASS__ . '::refresh() error: http code not 200. Responded: '.print_r($data, true);
+         $log = __CLASS__ . '::refresh() error: http code not 200. Responded: '.print_r($buffer, true);
          $return = false;
       } else {
+         $data = \json_decode($buffer, true);
          $this->auth = $data;
          $log = __CLASS__ . '::refresh() sets this->auth='.print_r($data, true);
          $return = true;
@@ -324,19 +359,31 @@ class GoogleHttpClient {
          return false;
       }
       $url = $this->token->get('revocation_endpoint');
+      //$curlPost = \http_build_query(array('token' => $access_token));
       $curlPost = 'token=' . $access_token;
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL, $url);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
       curl_setopt($ch, CURLOPT_POST, 1);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+      curl_setopt($ch, CURLOPT_CAINFO, $this->token->get('cert_path'));
+      //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+      //curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
+      curl_setopt($ch, CURLOPT_RESOLVE, [$this->token->get('accounts.google.com')]);
       curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
+      if ($this->log) {
+         curl_setopt($ch, CURLOPT_VERBOSE, true);
+         $fh = \fopen($this->token->get('curl_log'), 'a+');
+         curl_setopt($ch, CURLOPT_STDERR, $fh);
+      }
       $buffer = curl_exec($ch);
       $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
       curl_close($ch);
-      $data = \json_decode($buffer, true);
+      if ($this->log) {
+         \fclose($fh);
+      }
       if ($http_code != 200) {
-         $log = __CLASS__ . '::revokeToken() error: http code not 200. Responded: '.print_r($data, true);
+         $log = __CLASS__ . '::revokeToken() error: http code not 200. Responded: '.print_r($buffer, true);
          $return = false;
       } else {
          $this->auth = null;
@@ -359,6 +406,25 @@ class GoogleHttpClient {
     *    pass session data.
     *
     * postconditions: it sets property $user. On error, it returns false.
+    * Error response: Array (
+    *     [error] => Array
+    *         (
+    *             [errors] => Array
+    *                 (
+    *                     [0] => Array
+    *                         (
+    *                             [domain] => global
+    *                             [reason] => authError
+    *                             [message] => Invalid Credentials
+    *                             [locationType] => header
+    *                             [location] => Authorization
+    *                         )
+    * 
+    *                 )
+    *             [code] => 401
+    *             [message] => Invalid Credentials
+    *         )
+    *     )
     * 
     * @param string $access_token Google's authorization response under key 'access_token'
     * 
@@ -371,19 +437,34 @@ class GoogleHttpClient {
          return false;
       }
       $url = $this->token->get('userinfo_endpoint');
-      $ch = curl_init();      
+      $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL, $url);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '. $access_token));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+      curl_setopt($ch, CURLOPT_FRESH_CONNECT, TRUE);
+      curl_setopt($ch, CURLOPT_CAINFO, $this->token->get('cert_path'));
+      //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+      //curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+      curl_setopt($ch, CURLOPT_HEADER, TRUE);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:', 'Authorization: Bearer '. $access_token));
+      curl_setopt($ch, CURLOPT_RESOLVE, [$this->token->get('www.googleapis.com')]);
+      if ($this->log) {
+         curl_setopt($ch, CURLOPT_VERBOSE, true);
+         $fh = \fopen($this->token->get('curl_log'), 'a+');
+         curl_setopt($ch, CURLOPT_STDERR, $fh);
+      }
       $buffer = curl_exec($ch);
+      $info = curl_getinfo($ch);
       $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
       curl_close($ch);
-      $data = \json_decode($buffer, true);      
+      if ($this->log) {
+         \fclose($fh);
+      }
       if ($http_code != 200) {
-         $log = __CLASS__ . '::userInfo() error: http code not 200. Responded: '.print_r($data, true);
+         $log = __CLASS__ . '::userInfo() error: http code not 200. Responded: '.print_r($buffer, true);
          $return = false;
       } else {
+         $buffer = (isset($info["header_size"]))? \substr($buffer, $info["header_size"]) : "";
+         $data = \json_decode($buffer, true);
          $this->user = $data;
          $log = __CLASS__ . '::userInfo() sets this->user='.print_r($data, true);
          $return = true;
